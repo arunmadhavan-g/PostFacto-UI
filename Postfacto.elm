255249d1933c34395de68a1ba1e0b570b024002b
arunmadhavan-g <arunmadhavan.g@gmail.com>
2017-12-03 01:38:09 +0530

    Added more logic to delete and to clear the state once add is done and stuffs,

--- Postfacto.elm
+++ Postfacto.elm
@@ -5,13 +5,18 @@ import Html.Attributes exposing(..)
 import Html.Events exposing(..)
 import String exposing(..)
 
-
+type alias Post =
+    {
+        id: Int,
+        value : String
+    }
 type alias Model =
     {
         showHide : String,
         current :  String,
-        added : List(String)
+        added : List(Post)
     }
+
 
 model : Model
 model  =
@@ -23,36 +28,41 @@ model  =
 
 
 type Msg = Change String|
-            Add
+            Add String|
+            Delete Int
 
 update: Msg -> Model -> Model
 update msg model =
         case msg of
             Change content->
                 case ( content |> String.trim |> String.length) > 0 of
-                    True -> { model | showHide = "inline", current = content |> String.trim }
-                    False -> { model | showHide = "none" , current = content|> String.trim }
-            Add -> {model | showHide = "none", current = "", added = model.added ++ [model.current]}
-
+                    True -> { model | showHide = "inline", current = content}
+                    False -> { model | showHide = "none", current = content}
+            Add content -> {model | showHide = "none", current = "", added = model.added ++ [{id = (nextId model.added) ,value = content}]}
+            Delete id -> {model |  added = (List.filter (\a-> a.id /= id) model.added)}
 view : Model -> Html Msg
 view model =  div[]
               [
-                div [] (inputBox model.showHide),
+                div [] (inputBox model),
                 div [] [(addValues model.added)]
               ]
 
 main = Html.beginnerProgram { model = model, view = view, update = update }
 
-addValues : List(String) -> Html Msg
+addValues : List(Post) -> Html Msg
 addValues values =
-    div[] (List.map divForValue values)
+    div[] (List.map divForPost values)
 
-divForValue : String -> Html Msg
-divForValue value = div [] [span [] [text value]]
+divForPost : Post -> Html Msg
+divForPost post = div [] [span [] [text post.value], deleteButton post]
 
-inputBox : String -> List (Html Msg)
-inputBox showHide = [input [placeholder "Enter something...", onInput Change][text model.current],
-                     i[showHideStyle showHide, onClick Add ][text "Add"]]
+deleteButton: Post -> Html Msg
+deleteButton post = i [onClick (Delete post.id)][text "delete"]
+
+
+inputBox : Model -> List (Html Msg)
+inputBox model = [input [placeholder "Enter something...", onInput Change, value model.current][],
+                     i[showHideStyle model.showHide, onClick (Add model.current)][text "Add"]]
 
 showHideStyle: String  -> Attribute msg
 showHideStyle input =
@@ -60,3 +70,15 @@ showHideStyle input =
         [
             ("display", input)
         ]
+
+nextId : List(Post) -> Int
+nextId  posts  = case posts of
+                    [] -> 0
+                    _ ->  let
+                            head = (posts |> List.reverse |> List.head )
+                          in
+                            case head of
+                                Nothing -> 0
+                                Just val -> val.id + 1
+
+
